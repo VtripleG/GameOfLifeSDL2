80dc2f8479e3ccb9efd96131e69c5b2cdcf80d6a
VtripleG <eagleofdead1337@gmail.com>
2024-03-25 19:00:18 +0300

    Added methods for computing next gen

--- src/GameBoard.h
+++ src/GameBoard.h
@@ -1,16 +1,165 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
 
 class GameBoard
 {
 public:
     GameBoard(){}
 
-    GameBoard( int w, int h )
+    GameBoard( int width, int height )
     {
+        m_height = height;
+        m_width = width;
+        m_boardCurrent = new bool*[m_height];
+
+        for( size_t counter = 0; counter < m_height; ++counter )
+            m_boardCurrent[counter] = new bool [m_width];
+
+        m_boardFuture =  new bool*[m_height];
+
+        for( size_t counter = 0; counter < m_height; ++counter )
+            m_boardFuture[counter] = new bool [m_width];
+
+        for( size_t rowCounter = 0; rowCounter < m_height; ++rowCounter )
+            for( size_t columCounter = 0; columCounter < m_width; ++columCounter )
+                m_boardCurrent[rowCounter][columCounter] = false;
+    }
+
+    ~GameBoard()
+    {
+        delete [] m_boardCurrent;
+        delete [] m_boardFuture;
 
     }
 
-private:
+    void ComputeNextGen()
+    {
+        for( size_t rowCounter = 0; rowCounter < m_height; ++rowCounter )
+            for( size_t columCounter = 0; columCounter < m_width; ++columCounter )
+            {
+                int neighbourNum = NeighbourCounter( rowCounter, columCounter );
 
+                if( ( neighbourNum == 2 || neighbourNum == 3 ) && m_boardCurrent[rowCounter][columCounter] == true )
+                {
+                    m_boardFuture[rowCounter][columCounter] = true;
+                    continue;
+                }
+
+                if( neighbourNum == 3 && m_boardCurrent[rowCounter][columCounter] == false )
+                {
+                    m_boardFuture[rowCounter][columCounter] = true;
+                    continue;
+                }
+
+                    m_boardFuture[rowCounter][columCounter] = false;
+            }
+
+        m_boardCurrent = m_boardFuture;
+    }
+
+    bool** GetBoard()
+    {
+        return m_boardCurrent;
+    }
+
+    void AddStartPoint( int row, int colum )
+    {
+        m_boardCurrent[row][colum] = true;
+    }
+
+private:
+    int NeighbourCounter( int row, int colum )
+    {
+        int counterNeighbour = 0;
+
+        std::vector<int> rowList;
+        std::vector<int> columList;
+
+        if( row == 0 )
+        {
+            rowList.push_back( m_height - 1 );
+            rowList.push_back( m_height - 1 );
+            rowList.push_back( m_height - 1 );
+            rowList.push_back( row );
+            rowList.push_back( row + 1 );
+            rowList.push_back( row + 1 );
+            rowList.push_back( row + 1 );
+            rowList.push_back( row );
+        }
+
+        if( row == m_height - 1 )
+        {
+            rowList.push_back( row - 1 );
+            rowList.push_back( row - 1 );
+            rowList.push_back( row - 1 );
+            rowList.push_back( row );
+            rowList.push_back( 0 );
+            rowList.push_back( 0 );
+            rowList.push_back( 0 );
+            rowList.push_back( row );
+        }
+
+        if( (row < m_height - 1) && (row > 0) )
+        {
+            rowList.push_back( row - 1 );
+            rowList.push_back( row - 1 );
+            rowList.push_back( row - 1 );
+            rowList.push_back( row );
+            rowList.push_back( row + 1 );
+            rowList.push_back( row + 1 );
+            rowList.push_back( row + 1 );
+            rowList.push_back( row );
+        }
+
+        if( colum == 0 )
+        {
+            columList.push_back( m_width - 1 );
+            columList.push_back( colum );
+            columList.push_back( colum + 1 );
+            columList.push_back( colum + 1 );
+            columList.push_back( colum + 1 );
+            columList.push_back( colum );
+            columList.push_back( m_width - 1 );
+            columList.push_back( m_width - 1 );
+        }
+
+        if( colum == m_width - 1 )
+        {
+            columList.push_back( colum - 1 );
+            columList.push_back( colum );
+            columList.push_back( 0 );
+            columList.push_back( 0 );
+            columList.push_back( 0 );
+            columList.push_back( colum );
+            columList.push_back( colum - 1 );
+            columList.push_back( colum - 1 );
+        }
+
+        if( (colum < m_width - 1) && (colum > 0) )
+        {
+            columList.push_back( colum - 1 );
+            columList.push_back( colum );
+            columList.push_back( colum + 1 );
+            columList.push_back( colum + 1 );
+            columList.push_back( colum + 1 );
+            columList.push_back( colum );
+            columList.push_back( colum - 1 );
+            columList.push_back( colum - 1 );
+        }
+
+        for( size_t counter = 0; counter < 8; ++counter)
+        {
+            if ( m_boardCurrent[ rowList[ counter ] ][ columList[ counter ] ] == true )
+                ++counterNeighbour;
+        }
+
+        return counterNeighbour;
+    }
+private:
+    bool** m_boardCurrent;
+    bool** m_boardFuture;
+    int m_height;
+    int m_width;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 
+#include "GameBoard.h"
+
 int main()
 {
+    GameBoard board( 10, 10 );
+    board.AddStartPoint( 3, 3 );
+    board.AddStartPoint( 3, 4 );
+    board.AddStartPoint( 4, 3 );
+
+    bool** brd = board.GetBoard();
+
+    for ( size_t row = 0; row < 10; ++row )
+    {
+        for( size_t colum = 0; colum < 10; ++colum )
+            std::cout << '|' << brd[row][colum] << '|';
+
+        std::cout << std::endl;
+    }
+
+    for( size_t counter = 0; counter < 20; ++counter )
+        std::cout << '_';
+    std::cout << std::endl;
+
+    board.ComputeNextGen();
+
+    brd = board.GetBoard();
+
+    for ( size_t row = 0; row < 10; ++row )
+    {
+        for( size_t colum = 0; colum < 10; ++colum )
+            std::cout << '|' << brd[row][colum] << '|';
+
+        std::cout << std::endl;
+    }
+
+    board.~GameBoard();
     return 0;
 }
